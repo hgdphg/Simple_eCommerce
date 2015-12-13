7ecaae83a13bc827861b31f4b996c2e19863498c
hgdphg <hgdphg@gmail.com>
2015-12-14 02:36:43 +0700

    upgrade Cart feature

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:create]
+  before_action :reduce_cart, only: [:reduce]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -27,7 +28,8 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.add_product(product.id)
+    number = params[:number]
+    @line_item = @cart.add_product(product.id, number.to_f)
 
     respond_to do |format|
       if @line_item.save
@@ -59,7 +61,7 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to @line_item.cart, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,12 +1,26 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
 
-  def add_product(product_id)
+  def add_product(product_id, number)
     current_item = line_items.find_by(product_id: product_id)
     if current_item
-      current_item.quantity += 1
+      current_item.quantity += number
+      if current_item.quantity < 0
+        current_item.quantity =0
+      end
     else
       current_item = line_items.build(product_id: product_id)
+    end
+    current_item
+  end
+
+  def reduce_product(product_id)
+    current_item = line_items.find_by(product_id: product_id)
+    if current_item
+      current_item.quantity -= 1
+      if current_item.quantity < 0
+        current_item.quantity =0
+      end
     end
     current_item
   end
