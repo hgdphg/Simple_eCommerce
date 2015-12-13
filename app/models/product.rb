8b93781b4e2a73c39fc86500c5639216e8f81f97
hgdphg <hgdphg@gmail.com>
2015-12-13 20:48:55 +0700

    add cart feature (basic)

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,18 @@ class Product < ActiveRecord::Base
 
   validates :title, :description, :price, presence:true
   validates :price, :numericality => { :greater_than => 0 }
+
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Item present')
+        return false
+      end
+    end
 end
