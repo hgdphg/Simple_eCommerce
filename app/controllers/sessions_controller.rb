268f630cfdd3fb83680a03fa38182ca3e01c4803
hgdphg <hgdphg@gmail.com>
2015-12-19 01:45:49 +0700

    Authenticating user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+    id = session[:user_id]
+    if id
+      redirect_to admin_url
+    end
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      session[:username] = user.username
+      redirect_to admin_url
+    else
+      redirect_to login_url, alert: "Invalid username or password"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    session[:username] = nil
+    redirect_to store_url, notice: "Logged out"
+  end
+end
