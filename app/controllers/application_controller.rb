1f89dd6f581694f3e327127e0104b4ed8201f777
hgdphg <hgdphg@gmail.com>
2015-12-19 22:40:40 +0700

    implement limit access for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authorize
 
   include CurrentCart
   before_action :set_cart
+
+  protected
+    def authorize
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_path, notice: "Need login"
+      end
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
 
   has_secure_password
+
+  after_destroy :ensure_last_user
+
+  protected
+    def ensure_last_user
+      if User.count.zero?
+        raise "Can't destroy last user"
+      end
+    end
 end
